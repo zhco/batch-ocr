805ff732a6545b304591b0b8962cadccccf2a4fc
zhco <luycheung@qq.com>
2026-06-18 15:39:01 +0800

    Replace Kotlin with Java MainActivity

---
+++ android/app/src/main/java/com/marvis/batch_ocr/MainActivity.java
@@ -0,0 +1,11 @@
+package com.marvis.batch_ocr;
+
+import android.os.Bundle;
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
